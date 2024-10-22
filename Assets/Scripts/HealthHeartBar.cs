cfd81edcc2b1ce118d2a77ef7b1db2d1b32b334a
Eero Pokki <s2200880@edu.bc.fi>
2024-10-22 13:32:23 +0300

    Pelihahmo HP

---
+++ Assets/Scripts/HealthHeartBar.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHeartBar : MonoBehaviour
+{
+    public GameObject heartPrefab;
+    public PlayerHealth playerHealth;
+    List<HealthHeart> hearts = new List<HealthHeart>();
+
+
+    private void OnEnable()
+    {
+        PlayerHealth.OnPlayerDamaged += DrawHearts;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.OnPlayerDamaged += DrawHearts;
+    }
+
+    private void Start()
+    {
+        DrawHearts();
+    }
+    public void DrawHearts()
+    {
+        CleanHearts();
+
+        float maxHealthRemainder = playerHealth.maxHealth % 2;
+        int heartsToMake = (int)((playerHealth.maxHealth / 2) + maxHealthRemainder);
+        for (int i = 0; i < heartsToMake; i++) 
+        {
+            CreateEmptyHearts();
+        }
+
+        for (int i = 0; i < hearts.Count; i++) 
+        {
+            int heartStatusRemainder = (int)Mathf.Clamp(playerHealth.health - (i * 2), 0, 2);
+            hearts[i].SetHeartImage((HeartStatus)heartStatusRemainder);
+        }
+    }
+
+    public void CreateEmptyHearts()
+    {
+        GameObject newHeart = Instantiate(heartPrefab);
+        newHeart.transform.SetParent(transform);
+
+        HealthHeart heartCompnent = newHeart.GetComponent<HealthHeart>();
+        heartCompnent.SetHeartImage(HeartStatus.Empty);
+        hearts.Add(heartCompnent);
+    }
+
+    public void CleanHearts()
+    {
+        foreach (Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+        hearts = new List<HealthHeart>();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static event Action OnPlayerDamaged;
+    public static event Action OnPlayerDeath;
+
+    public float health, maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        OnPlayerDamaged?.Invoke();
+
+        if (health <= 0)
+        {
+            health = 0;
+            Debug.Log("You are DEAD!");
+            OnPlayerDeath?.Invoke();
+        }
+    }
+}
