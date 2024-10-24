ad2a4913c5ce6b1cb82c138251e42b0556b2ad09
PavenssiSal <s2200897@edu.bc.fi>
2024-10-24 11:51:17 +0300

    Controls menu kuvat
    Control menuun tehty kuvat Tiledillä

---
+++ Assets/Graphics/Tilesets/Pixel Keys x16/Pixel Keys x16/Tilemaps/pixel_keys_x16_white.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="pixel_keys_x16_white" tilewidth="16" tileheight="16" tilecount="140" columns="20">
+ <image source="pixel_keys_x16_white.png" width="320" height="112"/>
+</tileset>
---
+++ Assets/Map/Project Tower Crashers Map/pixel_keys_x16_black.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="pixel_keys_x16_black" tilewidth="16" tileheight="16" tilecount="140" columns="20">
+ <image source="../../Graphics/Tilesets/Pixel Keys x16/Pixel Keys x16/Tilemaps/pixel_keys_x16_black.png" width="320" height="112"/>
+</tileset>
